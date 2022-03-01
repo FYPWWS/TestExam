bd6015c80c88715f92e8235b60da7cff4c05a395
SIMON GABORIT - STUDENT <G00377155@gmit.ie>
2022-03-01 10:34:14 +0000

    First test

--- src/test/java/ie/gmit/TestStudent.java
+++ src/test/java/ie/gmit/TestStudent.java
@@ -1,2 +1,28 @@
-package ie.gmit;public class TestStudent {
+package ie.gmit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestStudent {
+
+    Student student;
+    @BeforeEach
+    void init()
+    {
+        student = new Student();
+    }
+
+    @Test
+    public void testName()
+    {
+        Assertions.assertFalse(student.Name.isEmpty());
+    }
+
+    @Test
+    public void testEmail()
+    {
+        Assertions.assertFalse(student.Email.isEmpty());
+    }
+
 }
